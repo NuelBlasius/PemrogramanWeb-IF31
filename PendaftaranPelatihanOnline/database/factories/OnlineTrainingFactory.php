74429893aee16dd7eb83cb179f7465b1ad9996c4
NuelBlasius <161033492+NuelBlasius@users.noreply.github.com>
2024-10-31 11:08:38 +0700

    Update OnlineTrainingFactory.php

--- PendaftaranPelatihanOnline/database/factories/OnlineTrainingFactory.php
+++ PendaftaranPelatihanOnline/database/factories/OnlineTrainingFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+// use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,20 @@ class OnlineTrainingFactory extends Factory
      */
     public function definition(): array
     {
+        // cara jalankan php artisan tinker
+        // App\Models\Pendaftaean::factory()->create(); untuk 1 data
+        // App\Models\Pendaftaean::factory(10)->create(); untuk 10 data
         return [
-            //
+            'participant_name' => fake()->name(),
+            'training_name' => fake()->word(),
+            'training_date' => fake()->date(),
+            'location' => fake()->address(),
+            'category' => fake()->randomElement(['child', 'teen', 'adult']),
+
+            // 'title' => fake()->sentence(),
+            // 'author' => fake()->name(),
+            // 'slug' => Str::slug(fake()->sentence()),
+            // 'body' => fake()->text()
         ];
     }
 }
